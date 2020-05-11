70821e8e14dfe0ed5b794eebe229518acaede9cc
Jorge Marzola <jorgemarzola@gmail.com>
2020-05-11 12:34:31 +0200

    Set up credit card charges with stripe

--- public/js/main.js
+++ public/js/main.js
@@ -1,47 +1,76 @@
 if (document.readyState == 'loading') {
-    document.addEventListener('DOMContentLoaded', ready);
+    document.addEventListener('DOMContentLoaded', ready)
 } else {
-    ready();
+    ready()
 }
 
 function ready() {
-    const removeCartItemButtons = document.getElementsByClassName('btn-danger');
+    const removeCartItemButtons = document.getElementsByClassName('btn-danger')
     for (let i = 0; i < removeCartItemButtons.length; i++) {
-        let button = removeCartItemButtons[i];
-        button.addEventListener('click', removeCartItem);
+        let button = removeCartItemButtons[i]
+        button.addEventListener('click', removeCartItem)
     }
 
-    const quantityInputs = document.getElementsByClassName('cart-quantity-input');
+    const quantityInputs = document.getElementsByClassName('cart-quantity-input')
     for (let i = 0; i < quantityInputs.length; i++) {
-        let input = quantityInputs[i];
-        input.addEventListener('change', quantityChanged);
+        let input = quantityInputs[i]
+        input.addEventListener('change', quantityChanged)
     }
 
-    const addToCartButtons = document.getElementsByClassName('shop-item-button');
+    const addToCartButtons = document.getElementsByClassName('shop-item-button')
     for (let i = 0; i < addToCartButtons.length; i++) {
-        let button = addToCartButtons[i];
-        button.addEventListener('click', addToCartClicked);
+        let button = addToCartButtons[i]
+        button.addEventListener('click', addToCartClicked)
     }
 
     document.getElementsByClassName('btn-purchase')[0].addEventListener('click',
-    purchaseClicked);
+    purchaseClicked)
 }
 
 let stripeHandler = StripeCheckout.configure({
     key: stripePublicKey,
     locale: 'auto',
     token: function(token) {
-        console.log(token)
+        let items = []
+        let cartItemContainer = document.getElementsByClassName('cart-items')[0]
+        let cartRows = cartItemContainer.getElementsByClassName('cart-row')
+        for (let i = 0; i < cartRows.length; i++) {
+            let cartRow = cartRows[i]
+            let quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
+            let quantity = quantityElement.value
+            let id = cartRow.dataset.itemId
+            items.push({
+                id: id,
+                quantity: quantity
+            })
+        }
+
+        fetch('/compra', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify({
+                stripeTokenId: token.id,
+                items: items
+            })
+        }).then(function(res) {
+            return res.json()
+        }).then(function(data) {
+            alert(data.message)
+            let cartItems = document.getElementsByClassName('cart-items')[0]
+            while (cartItems.hasChildNodes()) {
+                cartItems.removeChild(cartItems.firstChild)
+            }
+            updateCartTotal()
+        }).catch(function(error) {
+            console.error(error)
+        })
     }
 })
 
 function purchaseClicked() {
-    // alert('Gracias por su compra');
-    // let cartItems = document.getElementsByClassName('cart-items')[0];
-    // while (cartItems.hasChildNodes()) {
-    //     cartItems.removeChild(cartItems.firstChild);
-    // }
-    // updateCartTotal();
     let priceElement = document.getElementsByClassName('cart-total-price')[0]
     let price = parseFloat(priceElement.innerText.replace('$', '')) * 100
     stripeHandler.open({
@@ -50,32 +79,34 @@ function purchaseClicked() {
 }
 
 function quantityChanged(e) {
-    let input = e.target;
+    let input = e.target
     if (isNaN(input.value) || input.value <= 0) {
-        input.value = 1;
+        input.value = 1
     }
-    updateCartTotal();
+    updateCartTotal()
 }
 
 function addToCartClicked(e) {
-    let button = e.target;
-    let shopItem = button.parentElement.parentElement;
-    let title = shopItem.getElementsByClassName('shop-item-title')[0].innerText;
-    let price = shopItem.getElementsByClassName('shop-item-price')[0].innerText;
-    let imageSrc = shopItem.getElementsByClassName('shop-item-image')[0].src;
-    addItemToCart(title, price, imageSrc);
-    updateCartTotal();
+    let button = e.target
+    let shopItem = button.parentElement.parentElement
+    let title = shopItem.getElementsByClassName('shop-item-title')[0].innerText
+    let price = shopItem.getElementsByClassName('shop-item-price')[0].innerText
+    let imageSrc = shopItem.getElementsByClassName('shop-item-image')[0].src
+    let id = shopItem.dataset.itemId
+    addItemToCart(title, price, imageSrc, id)
+    updateCartTotal()
 }
 
-function addItemToCart(title, price, imageSrc) {
-    let cartRow = document.createElement('div');
-    cartRow.classList.add('cart-row');
-    let cartItems = document.getElementsByClassName('cart-items')[0];
-    let cartItemsNames = cartItems.getElementsByClassName('cart-item-title');
+function addItemToCart(title, price, imageSrc, id) {
+    let cartRow = document.createElement('div')
+    cartRow.classList.add('cart-row')
+    cartRow.dataset.itemId = id
+    let cartItems = document.getElementsByClassName('cart-items')[0]
+    let cartItemsNames = cartItems.getElementsByClassName('cart-item-title')
     for (let i = 0; i < cartItemsNames.length; i++) {
         if (cartItemsNames[i].innerText == title) {
-            alert('Este item ya se encuentra en el carrito de compras');
-            return;
+            alert('Este item ya se encuentra en el carrito de compras')
+            return
         }
     }
     let cartRowContents = `  
@@ -87,35 +118,35 @@ function addItemToCart(title, price, imageSrc) {
         <div class="cart-quantity cart-column">
             <input class="cart-quantity-input" type="number" value="1">
             <button class="btn btn-danger" role="button">REMOVER</button>
-        </div>`;
-    cartRow.innerHTML = cartRowContents;
-    cartItems.append(cartRow);
+        </div>`
+    cartRow.innerHTML = cartRowContents
+    cartItems.append(cartRow)
     // Event listeners need to be added again as these elements were not 
     // loaded when our JS did
     cartRow.getElementsByClassName('btn-danger')[0].addEventListener
-    ('click', removeCartItem);
+    ('click', removeCartItem)
     cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener
-    ('change', quantityChanged);
+    ('change', quantityChanged)
 }
 
 function removeCartItem(e) {
-    let buttonClicked = e.target;
-    buttonClicked.parentElement.parentElement.remove();
-    updateCartTotal();
+    let buttonClicked = e.target
+    buttonClicked.parentElement.parentElement.remove()
+    updateCartTotal()
 }
 
 function updateCartTotal() {
-    let cartItemContainer = document.getElementsByClassName('cart-items')[0];
-    let cartRows = cartItemContainer.getElementsByClassName('cart-row');
-    let total = 0;
+    let cartItemContainer = document.getElementsByClassName('cart-items')[0]
+    let cartRows = cartItemContainer.getElementsByClassName('cart-row')
+    let total = 0
     for (let i = 0; i < cartRows.length; i++) {
-        let cartRow = cartRows[i];
-        let priceElement = cartRow.getElementsByClassName('cart-price')[0];
-        let quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0];
+        let cartRow = cartRows[i]
+        let priceElement = cartRow.getElementsByClassName('cart-price')[0]
+        let quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
         // parseFloat converts the string into a float
-        let price = parseFloat(priceElement.innerText.replace('$', ''));
-        let quantity = quantityElement.value;
-        total = total + (price * quantity);
+        let price = parseFloat(priceElement.innerText.replace('$', ''))
+        let quantity = quantityElement.value
+        total = total + (price * quantity)
     }
     // Round to two decimal places
     total = Math.round(total * 100) / 100;
--- server.js
+++ server.js
@@ -10,8 +10,11 @@ console.log(stripeSecretKey, stripePublicKey)
 const express = require('express')
 const app = express()
 const fs = require('fs')
+const stripe = require('stripe')(stripeSecretKey)
+
 
 app.set('view engine', 'ejs')
+app.use(express.json())
 app.use(express.static('public'))
 
 app.get('/tienda', function(req, res) {
@@ -22,6 +25,36 @@ app.get('/tienda', function(req, res) {
       res.render('tienda.ejs', {
         stripePublicKey: stripePublicKey,
         items: JSON.parse(data)
+      })
+    }
+  })
+})
+
+app.post('/compra', function(req, res) {
+  fs.readFile('items.json', function(error, data) {
+    if (error) {
+      res.status(500).end()
+    } else {
+      const itemsJson = JSON.parse(data)
+      const itemsArray = itemsJson.music.concat(itemsJson.merch)
+      let total = 0
+      req.body.items.forEach(function(item) {
+        const itemJson = itemsArray.find(function(i) {
+          return i.id == item.id
+        })
+        total = total + itemJson.price * item.quantity
+      })
+
+      stripe.charges.create({
+        amount: total,
+        source: req.body.stripeTokenId,
+        currency: 'usd'
+      }).then(function() {
+        console.log('Charge Succesful')
+        res.json({message: 'Gracias por su compra'})
+      }).catch(function() {
+        console.log('Charge Fail')
+        res.status(500).end()
       })
     }
   })
